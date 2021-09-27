aa874f22f92103578713e248d261d7a420bbda04
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-09-27 13:23:18 -0300

    Starting work on controller

--- DollarPricing/src/main/java/br/com/leomanzini/dollar/price/controller/DollarPriceController.java
+++ DollarPricing/src/main/java/br/com/leomanzini/dollar/price/controller/DollarPriceController.java
@@ -1,10 +1,22 @@
 package br.com.leomanzini.dollar.price.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.leomanzini.dollar.price.dto.DollarResponse;
+import br.com.leomanzini.dollar.price.service.DollarPriceService;
 
 @RestController
 @RequestMapping("/price")
 public class DollarPriceController {
 	
+	@Autowired
+	private DollarPriceService dollarService;
+	
+	@GetMapping
+	public DollarResponse getDollarPrice() {
+		return dollarService.getPrice(null);
+	}
 }
--- DollarPricing/src/main/java/br/com/leomanzini/dollar/price/service/DollarPriceService.java
+++ DollarPricing/src/main/java/br/com/leomanzini/dollar/price/service/DollarPriceService.java
@@ -1,9 +1,17 @@
 package br.com.leomanzini.dollar.price.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.leomanzini.dollar.price.dto.DollarResponse;
 
 @Service
 public class DollarPriceService {
 	
+	@Autowired
+	private DollarPriceRepository dollarRepository;
 	
+	public DollarResponse getPrice(Long id) {
+		return dollarRepository.getById(id);
+	}
 }
