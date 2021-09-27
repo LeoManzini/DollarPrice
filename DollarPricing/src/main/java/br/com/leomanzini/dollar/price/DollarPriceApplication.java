37e5ed09a68c95ab9dbcebfe8177c865568ddefc
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-09-27 10:13:02 -0300

    Adding the empty source code to the repository

---
+++ DollarPricing/src/main/java/br/com/leomanzini/dollar/price/DollarPriceApplication.java
@@ -0,0 +1,13 @@
+package br.com.leomanzini.dollar.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarPriceApplication.class, args);
+	}
+
+}
---
+++ DollarPricing/src/test/java/br/com/leomanzini/dollar/price/DollarPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leomanzini.dollar.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
