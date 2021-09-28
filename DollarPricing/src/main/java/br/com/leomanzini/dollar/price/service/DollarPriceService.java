0d227cffdc65888b48e4214c2fb0c671606e54ad
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-09-28 11:14:58 -0300

    Refactoring and working code

--- DollarPricing/src/main/java/br/com/leomanzini/dollar/price/service/DollarPriceService.java
+++ DollarPricing/src/main/java/br/com/leomanzini/dollar/price/service/DollarPriceService.java
@@ -2,15 +2,14 @@ package br.com.leomanzini.dollar.price.service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Type;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import br.com.leomanzini.dollar.price.dto.response.HistoryDollarResponse;
