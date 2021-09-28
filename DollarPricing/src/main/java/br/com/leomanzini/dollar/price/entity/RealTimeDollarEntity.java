a6234dd4eb6a60b4445806182b7f727e0c26bad9
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-09-28 16:00:12 -0300

    Starting work with profiles

--- DollarPricing/src/main/java/br/com/leomanzini/dollar/price/entity/RealTimeDollarEntity.java
+++ DollarPricing/src/main/java/br/com/leomanzini/dollar/price/entity/RealTimeDollarEntity.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +16,8 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "realtime")
+@Entity
+@Table(name = "realtime")
 public class RealTimeDollarEntity {
 	
 	@Id
